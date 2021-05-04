6c515caeae5271a95182afd7dd48f56f0fcca709
snowhawk04 <6187693+snowhawk04@users.noreply.github.com>
2021-05-04 16:01:14 -0700

    Update BaseFilter.cs

--- Filter/BaseFilter.cs
+++ Filter/BaseFilter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Stashie
 {
@@ -8,7 +9,7 @@ namespace Stashie
         public bool BAny { get; set; }
         public bool CompareItem(ItemData itemData)
         {
-            return BAny ? Filters.Any(x => x.CompareItem(itemData)) : Filters.AllF(x => x.CompareItem(itemData));
+            return BAny ? Filters.Any(x => x.CompareItem(itemData)) : Filters.All(x => x.CompareItem(itemData));
         }
     }
 }
