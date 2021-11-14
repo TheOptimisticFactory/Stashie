52f061c56a3df5dd6e8e2bafae926facf7172c28
TheOptimisticFactory <romain.gagnaire.tm@gmail.com>
2021-11-14 22:57:26 +0100

    Added ScourgeTier Keyword

--- ItemData.cs
+++ ItemData.cs
@@ -48,6 +48,7 @@ namespace Stashie
         public int DeliriumStacks { get; }
 
         //public int ClusterJewelpassives { get; }
+        public int ScourgeTier { get; }
         public bool BIdentified { get; }
         public bool isCorrupted { get; }
         public bool isElder { get; }
@@ -85,6 +86,7 @@ namespace Stashie
             isWarlord = baseComponent.isWarlord;
             isHunter = baseComponent.isHunter;
             isInfluenced = isCrusader || isRedeemer || isWarlord || isHunter || isShaper || isElder;
+            ScourgeTier = baseComponent.ScourgedTier;
 
             var mods = item.GetComponent<Mods>();
             Rarity = mods?.ItemRarity ?? ItemRarity.Normal;
